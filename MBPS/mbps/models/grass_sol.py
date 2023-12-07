5e57412fe064e08fbab968122e8b1f6a15864a85
agerardursin <32272732+agerardursin@users.noreply.github.com>
2023-12-07 14:48:30 +0100

    Update grass_sol.py

--- MBPS/mbps/models/grass_sol.py
+++ MBPS/mbps/models/grass_sol.py
@@ -5,238 +5,230 @@ MSc Biosystems Engineering, WUR
 @authors:   Daniel Reyes Lastiri, Stefan Maranus,
             Rachel van Ooteghem, Tim Hoogstad
 
-Class for grass growth model from Grasim
+Evaluation of the grass & water model
 """
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import colormaps
 import sys
-sys.path.append('../mbps/classes/')
-sys.path.append('../mbps/functions/')
+sys.path.append('../mbps/models/')
+from models.grass_sol import Grass
+from models.water_sol import Water
+from scipy.optimize import least_squares
 
-from classes.module import Module
-from functions.integration import fcn_euler_forward
+from models.grass_sol import Grass
+from functions.calibration import fcn_residuals, fcn_accuracy
 
-class Grass(Module):
-    ''' 
-    Grass growth from Grasim model by Mohtar et al. (1997).
-    Model time units in [d].
+plt.style.use('ggplot')
+
+# Simulation time
+tsim = np.linspace(0, 365, int(365/5)+1) # [d]
+# tsim = np.linspace(0, 365*2, int(365/5)+1) # [d]
+
+# Weather data (disturbances shared across models)
+t_ini = '19950101'
+# t_ini = '19940101'
+t_end = '19960101'
+# t_weather = np.linspace(0, 365*2, 365*2+1)
+t_weather = np.linspace(0, 365, 365+1)
+data_weather = pd.read_csv(
+    '../data/etmgeg_260.csv', # .. to move up one directory from current directory
+    skipinitialspace=True, # ignore spaces after comma separator
+    header = 47-3, # row with column names, 0-indexed, excluding spaces
+    usecols = ['YYYYMMDD', 'TG', 'Q', 'RH'], # columns to use
+    # usecols = ['YEAR','MO', 'DY', 'TG', 'Q', 'RH'],
+    index_col = 0, # column with row names from used columns, 0-indexed
+    )
+# t_data = np.array([107, 114, 122, 129, 136, 142, 149, 156])+365
+t_data = np.array([107, 114, 122, 129, 136, 142, 149, 156])
+m_data = np.array([156., 198., 333., 414., 510., 640., 663., 774.])
+m_data = m_data/1E3
+
+# -- Grass sub-model --
+# Step size
+dt_grs = 1 # [d]
+
+# Initial conditions
+# TODO: Specify suitable initial conditions for the grass sub-model
+x0_grs = {'Ws':1.0/50,'Wg':1.5/50} # [kgC m-2]
+
+# Model parameters (as provided by Mohtar et al. 1997 p.1492-1493)
+p_grs = {'a':40.0,          # [m2 kgC-1] structural specific leaf area
+     'alpha':2E-9,      # [kgCO2 J-1] leaf photosynthetic efficiency
+     'beta':0.05,       # [d-1] senescence rate 
+     'k':0.5,           # [-] extinction coefficient of canopy
+     'm':0.1,           # [-] leaf transmission coefficient
+     'M':0.02,          # [d-1] maintenance respiration coefficient
+     'mu_m':0.5,        # [d-1] max. structural specific growth rate
+     'P0':0.432,        # [kgCO2 m-2 d-1] max photosynthesis parameter
+     'phi':0.9,         # [-] photoshynth. fraction for growth
+     'Tmin':0.0,        # [°C] maximum temperature for growth
+     'Topt':20.0,       # [°C] minimum temperature for growth
+     'Tmax':42.0,       # [°C] optimum temperature for growth
+     'Y':0.75,          # [-] structure fraction from storage
+     'z':1.33           # [-] bell function power
+     }
+
+
+# Model parameters adjusted manually to obtain growth
+# TODO: Adjust a few parameters to obtain growth.
+# Satrt by using the modifications from Case 1.
+# If needed, adjust further those or additional parameters
+p_grs['alpha'] = 8E-9#8.346E-09#4.478E-9#4.75E-9#4.009E-09
+p_grs['beta'] = 0.04145# #directly from grass_cal output
+p_grs['k'] = 0.18#0.18#0.1757
+p_grs['m'] = 0.8#0.8#0.6749
+p_grs['phi'] = 0.85#8.591E-01+0.1
+
+# Disturbances
+# PAR [J m-2 d-1], environment temperature [°C], leaf area index [-]
+T = data_weather.loc[t_ini:t_end,'TG'].values    # [0.1 °C] Env. temperature
+I_gl = data_weather.loc[t_ini:t_end,'Q'].values  # [J cm-2 d-1] Global irr.
+
+T = T/10                   # [0.1 °C] to [°C] Environment temperature
+I0 = 0.45*I_gl*1E4/dt_grs  # [J cm-2 d-1] to [J m-2 d-1] Global irr. to PAR
+
+d_grs = {'T':np.array([t_weather, T]).T,
+    'I0':np.array([t_weather, I0]).T,   
+    }
+
+# Initialize module
+grass = Grass(tsim, dt_grs, x0_grs, p_grs)
+
+# -- Water sub-model --
+dt_wtr = 1 # [d]
+
+# Initial conditions
+# TODO: Specify suitable initial conditions for the soil water sub-model
+x0_wtr = {'L1':0.36*150, 'L2':0.32*250, 'L3':0.24*600, 'DSD':1} # 3*[mm], [d]
+
+# Castellaro et al. 2009, and assumed values for soil types and layers
+p_wtr = {'alpha':1.29E-6,   # [mm J-1] Priestley-Taylor parameter
+     'gamma':0.68,      # [mbar °C-1] Psychrometric constant
+     'alb':0.23,        # [-] Albedo (assumed constant crop & soil)
+     'kcrop':0.85,      # [mm d-1] Evapotransp coefficient, range (0.85-1.0)
+     'WAIc':0.75,       # [-] WDI critical, range (0.5-0.8)
+     'theta_fc1':0.36,      # [-] Field capacity of soil layer 1
+     'theta_fc2':0.32,      # [-] Field capacity of soil layer 2
+     'theta_fc3':0.24,      # [-] Field capacity of soil layer 3
+     'theta_pwp1':0.21,     # [-] Permanent wilting point of soil layer 1 
+     'theta_pwp2':0.17,     # [-] Permanent wilting point of soil layer 2
+     'theta_pwp3':0.10,     # [-] Permanent wilting point of soil layer 3
+     'D1':150,        # [mm] Depth of Soil layer 1
+     'D2':250,        # [mm] Depth of soil layer 2
+     'D3':600,        # [mm] Depth of soil layer 3
+     'krf1':0.25,     # [-] Rootfraction layer 1 (guess)
+     'krf2':0.50,     # [-] Rootfraction layer 2 (guess)
+     'krf3':0.25,     # [-] Rootfraction layer 2 (guess)
+     'mlc':0.2,       # [-] Fraction of soil covered by mulching
+     'S':10,          # [mm d-1] parameter of precipitation retention
+     }
+
+# Disturbances
+# global irradiance [J m-2 d-1], environment temperature [°C], 
+# precipitation [mm d-1], leaf area index [-].
+T = data_weather.loc[t_ini:t_end,'TG'].values      # [0.1 °C] Env. temperature
+I_glb = data_weather.loc[t_ini:t_end,'Q'].values  # [J cm-2 d-1] Global irr. 
+f_prc = data_weather.loc[t_ini:t_end,'RH'].values # [0.1 mm d-1] Precipitation
+f_prc[f_prc<0.0] = 0 # correct data that contains -0.1 for very low values
+
+T = T/10                # [0.1 °C] to [°C] Environment temperature
+I_glb = I_glb*1E4/dt_wtr    # [J cm-2 d-1] to [J m-2 d-1] Global irradiance
+f_prc = f_prc/10/dt_wtr     # [0.1 mm d-1] to [mm d-1] Precipitation
+
+d_wtr = {'I_glb' : np.array([t_weather, I_glb]).T, 
+    'T' : np.array([t_weather, T]).T,
+    'f_prc': np.array([t_weather, f_prc]).T,
+     }
+
+# Initialize module
+water = Water(tsim, dt_wtr, x0_wtr, p_wtr)
+
+#%% Simulation function
+def fnc_y(p0):
+    # Reset initial conditions
+    grass.x0 = x0_grs.copy()
+    water.x0 = x0_wtr.copy()
     
-    Parameters
-    ----------
-    tsim : array
-        A sequence of time points for the simulation [d]
-    dt : scalar
-        Time step for the numerical integration [d]
-    x0 : dictionary of floats
-        Initial conditions of the state variables
-        
-        ======  =============================================================
-        key     meaning
-        ======  =============================================================
-        'Ws'    [kgC m-2] Storage weight
-        'Wg'    [kgC m-2] Structure weight
-        ======  =============================================================
-        
-    p : dictionary of floats
-        Model parameters
-        
-        =======  ============================================================
-        key      meaning
-        =======  ============================================================
-        'a'      [m2 kgC-1] structural specific leaf area
-        'alpha'  [kgCO2 J-1] leaf photosynthetic efficiency
-        'beta'   [d-1] senescence rate
-        'k'      [-] extinction coefficient of canopy
-        'm'      [-] leaf transmission coefficient
-        'M'      [d-1] maintenance respiration coefficient
-        'mu_m'   [d-1] max. structural specific growth rate
-        'P0'     [kgCO2 m-2 d-1] max photosynthesis parameter
-        'phi'    [-] photoshynthetic fraction for growth
-        'Tmax'   [°C] maximum temperature for growth
-        'Tmin'   [°C] minimum temperature for growth
-        'Topt'   [°C] optimum temperature for growth
-        'Y'      [-] structure fraction from storage
-        'z'      [-] bell function power
-        =======  ============================================================
+    # grass.p['alpha'] = p0[0]
+    # grass.p['phi'] = p0[0]#p0[1]
+    grass.p['alpha'] = p0[0]
+    # grass.p['phi'] = p0[1]
+    water.p['kcrop'] = p0[1]#p0[2]
+    water.p['krf3'] = p0[2]
+
+    # Initial disturbance
+    d_grs['WAI'] = np.array([[0,1,2,3,4], [1.,]*5]).T
     
-    d : dictionary of 2D arrays
-        Model disturbances (required for method 'run'),
-        of shape (len(t_d),2) for time and disturbance.
-        
-        =======  ============================================================
-        key      meaning
-        =======  ============================================================
-        'I_gl'   [J m-2 d-1] Global irradiance
-        'T'      [°C] Environment average daily temperature
-        'WAI'    [-] Water availability index
-        =======  ============================================================        
-    
-    u : dictionary of 2D arrays
-        Controlled inputs (required for method 'run'),
-        of shape (len(t_d),2) for time and controlled input.
-        
-        =======  ============================================================
-        key      meaning
-        =======  ============================================================
-        'f_Hr'   [kgC m-2 d-1] Harvesting rate
-        'f_Gr'   [kgC m-2 d-1] Grazing rate
-        =======  ============================================================ 
-    
-    Returns
-    -------
-    y : dictionary of arrays
-        Model outputs (returned by method 'run')
-        
-        =======  ============================================================
-        key      meaning
-        =======  ============================================================
-        'Ws'     [kgC m-2] Storage weight
-        'Wg'     [kgC m-2] Structure weight
-        'LAI'    [-] Leaf area index
-        =======  ============================================================
-        
-    f : dictionary of arrays
-        Mass flow rates (generated by method 'run')
-        
-        =======  ============================================================
-        key      meaning
-        =======  ============================================================
-        'f_P'    [kgC m-2 d-1] Photosynthesis
-        'f_SR'   [kgC m-2 d-1] Shoot respiration
-        'f_G'    [kgC m-2 d-1] Shoot growth
-        'f_MR'   [kgC m-2 d-1] Maintenance respiration
-        'f_R'    [kgC m-2 d-1] Recycling
-        'f_S'    [kgC m-2 d-1] Senescence
-        'f_Hr'   [kgC m-2 d-1] Harvesting rate
-        'f_Gr'   [kgC m-2 d-1] Grazing rate
-        =======  ============================================================
-        
-        
-    References
-    ----------
-    * Mohtar, R. H., Buckmaster, D. R., & Fales, S. L. (1997),
-      A grazing simulation model: GRASIM A: Model development,
-      Transactions of the ASAE, 40(5), 1483-1493.
-    
-    * Johnson, I. R., and J. H. M. Thornley (1983).
-      Vegetative crop growth model incorporating leaf area expansion and
-      senescence, and applied to grass, 
-      Plant, Cell & Environment 6.9, 721-729.
-    '''
-    def __init__(self, tsim, dt, x0, p):
-        Module.__init__(self, tsim, dt, x0, p)
-        # Initialize dictionary of flows
-        self.f = {}
-        self.f_keys = ('f_P', 'f_SR', 'f_G', 'f_MR',
-                       'f_R', 'f_S', 'f_Hr', 'f_Gr')
-        for k in self.f_keys:
-            self.f[k] = np.full((self.t.size,), np.nan)
-    
-    def diff(self, _t, _x0):
-        # -- Initial conditions
-        Ws, Wg = _x0[0], _x0[1]
-        
-        # -- Physical constants
-        theta = 12/44            # [-] CO2 to C (physical constant)
-        
-        # -- Model parameteres
-        a = self.p['a']          # [m2 kgC-1] structural specific leaf area
-        alpha = self.p['alpha']  # [kgCO2 J-1] leaf photosynthetic efficiency
-        beta = self.p['beta']    # [d-1] senescence rate
-        k = self.p['k']          # [-] extinction coefficient of canopy
-        m = self.p['m']          # [-] leaf transmission coefficient
-        M = self.p['M']          # [d-1] maintenance respiration coefficient
-        mu_m = self.p['mu_m']    # [d-1] max. structural specific growth rate
-        P0 = self.p['P0']        # [kgCO2 m-2 d-1] max photosynthesis parameter
-        # P1 = self.p['P1']      # [kgCO2 m-2 d-1 °C-1] max photosynthesis parameter
-        phi = self.p['phi']      # [-] photoshynth. fraction for growth
-        Tmax = self.p['Tmax']    # [°C] maximum temperature for growth
-        Tmin = self.p['Tmin']    # [°C] minimum temperature for growth
-        Topt = self.p['Topt']    # [°C] optimum temperature for growth
-        Y = self.p['Y']          # [-] structure fraction from storage
-        z = self.p['z']          # [-] bell function power
-        
-        # -- Disturbances at instant _t
-        I0, T, WAI = self.d['I0'], self.d['T'], self.d['WAI']
-        _I0 = np.interp(_t,I0[:,0],I0[:,1])     # [J m-2 d-2] PAR
-        _T = np.interp(_t,T[:,0],T[:,1])        # [°C] Environment temperature
-        _WAI = np.interp(_t,WAI[:,0],WAI[:,1])  # [-] Water availability index
-        
-        # -- Controlled inputs
-        f_Gr = self.u['f_Gr']    # [kgC m-2 d-1] Graze
-        f_Hr = self.u['f_Hr']    # [kgC m-2 d-1] Harvest
-        
-        # -- Supporting equations
-        # - Mass
-        W = Ws + Wg             # [kgC m-2] total mass
-        # - Temperature index [-]
-        DTmax = max(Tmax - _T, 0)
-        DTmin = max(_T - Tmin, 0)
-        DTa = Tmax-Topt
-        DTb = Topt-Tmin
-        TI = (DTmax/DTa * (DTmin/DTb)**(DTb/DTa))**z
-        # - Photosynthesis
-        LAI = a*Wg                      # [m2 m-2] Leaf area index
-        if TI==0 and _I0==0:
-            P = 0.0
-        else:
-            Pm = P0*TI                      # [kgCO2 m-2 d-1] Max photosynthesis
-            C1 = alpha*k*_I0/(1-m)          # [kgCO2 m-2 d-1]
-            C2 = (C1+Pm)/(C1*np.exp(-k*LAI)+Pm) # [-]
-            P = Pm/k*np.log(C2)             # [kgCO2 m-2 d-1] Photosynthesis rate
-        # - Flows
-        # Photosynthesis [kgC m-2 d-1]
-        f_P = _WAI*phi*theta*P
-        # Shoot respiration [kgC m-2 d-1]
-        f_SR = (1-Y)/Y * mu_m*Ws*Wg/W
-        # Maintenance respiration [kgC m-2 d-1]
-        f_MR = M*Wg
-        # Growth [kgC m-2 d-1]
-        f_G = mu_m*Ws*Wg/W
-        # Senescence [kgC m-2 d-1]
-        f_S = beta*Wg
-        # Recycling (can solve Ws<0, remove for student version)
-        if Ws<1E-5:
-            f_R = 0.01*Wg
-        else:
-            f_R = 0
-        # f_R = 0
-        
-        # -- Differential equations [kgC m-2 d-1]
-        dWs_dt = f_P - f_SR - f_G - f_MR + f_R
-        dWg_dt = f_G - f_S - f_R - f_Hr - f_Gr
-        
-        # -- Store flows [kgC m-2 d-1]
-        idx = np.isin(np.round(self.t,8), np.round(_t,8))
-        self.f['f_P'][idx] = f_P
-        self.f['f_SR'][idx] = f_SR
-        self.f['f_G'][idx] = f_G
-        self.f['f_MR'][idx] = f_MR
-        self.f['f_R'][idx] = f_R
-        self.f['f_S'][idx] = f_S
-        self.f['f_Hr'][idx] = f_Hr
-        self.f['f_Gr'][idx] = f_Gr
-        
-        return np.array([dWs_dt,dWg_dt])
-    
-    def output(self, tspan):
-        # Retrieve object properties
-        dt = self.dt        # integration time step size
-        diff = self.diff    # function with system of differential equations
-        Ws0 = self.x0['Ws'] # initial condition
-        Wg0 = self.x0['Wg'] # initial condiiton
-        a = self.p['a']
-        # Numerical integration
-        y0 = np.array([Ws0,Wg0])
-        y_int = fcn_euler_forward(diff,tspan,y0,h=dt)
-        # Model results
-        # assuming 0.4 kgC/kgDM (Mohtar et al. 1997, p. 1492)
-        t = y_int['t']
-        Ws = y_int['y'][0,:]
-        Wg = y_int['y'][1,:]
-        LAI = a*Wg
-        return {
-            't':t,          # [d] Integration time
-            'Ws':Ws,        # [kgC m-2] Structure weight 
-            'Wg':Wg,        # [kgC m-2] Storage weight 
-            'LAI':LAI,      # [-] Leaf area index
-        }
+    # Iterator
+    # (stop at second-to-last element, and store index in Fortran order)
+    it = np.nditer(tsim[:-1], flags=['f_index'])
+    for ti in it:
+        # Index for current time instant
+        idx = it.index
+        # Integration span
+        tspan = (tsim[idx], tsim[idx+1])
+        # Controlled inputs
+        u_grs = {'f_Gr':0, 'f_Hr':0}   # [kgDM m-2 d-1]
+        u_wtr = {'f_Irg':0}            # [mm d-1]
+        # Run grass model
+        y_grs = grass.run(tspan, d_grs, u_grs)
+        # Retrieve grass model outputs for water model
+        d_wtr['LAI'] = np.array([y_grs['t'], y_grs['LAI']])
+        # Run water model    
+        y_wtr = water.run(tspan, d_wtr, u_wtr)
+        # Retrieve water model outputs for grass model
+        d_grs['WAI'] = np.array([y_wtr['t'], y_wtr['WAI']])
+   
+    # Return result of interest (WgDM [kgDM m-2])
+    return grass.y['Wg']/0.4
+
+#### -- Calibration --
+
+# Run calibration function
+# p0 = np.array([p_grs['alpha'], p_grs['beta']]) # Initial guess
+# bnds = ((1E-12, 1E-4), (1E-3, 0.1))
+# p0 = np.array([p_grs['alpha'], p_grs['phi'], p_grs['beta']]) # Initial guess
+# bnds = ((1E-12, 0.1, 1E-4), (1E-3, 0.99, 0.1))
+
+#Final
+# p0 = np.array([p_grs['alpha'], p_grs['phi']]) # Initial guess
+# bnds = ((1E-12, 1E-4), (1E-3, 0.99))
+
+#%% Challenge
+# p0 = np.array([p_grs['alpha'], p_grs['m'], p_grs['phi'], p_wtr['kcrop']]) # Initial guess
+# bnds = ((1E-12, 1E-4, 0.1, 0.85), (1E-3, 0.8, 0.99, 1))
+# p0 = np.array([p_grs['m'], p_grs['phi'], p_wtr['kcrop']]) # Initial guess
+# bnds = ((1E-4, 0.1, 0.85), (0.8, 0.99, 1))
+# p0 = np.array([p_grs['alpha'], p_wtr['kcrop']]) # Initial guess
+# bnds = ((1E-12, 0.85), (1E-3, 1))
+# p0 = np.array([p_grs['alpha'], p_grs['phi'], p_wtr['kcrop'], p_wtr['krf3']]) # Initial guess
+# bnds = ((1E-12, 1E-3, 0.85, 0.1), (1E-3, 0.99, 1, 0.75))
+# p0 = np.array([p_grs['alpha'], p_grs['phi'], p_wtr['kcrop'], p_wtr['krf3']]) # Initial guess
+# bnds = ((1E-12, 1E-3, 0.85, 0.1), (1E-3, 0.99, 1, 0.75))
+p0 = np.array([p_grs['alpha'], p_wtr['kcrop'], p_wtr['alpha']]) # Initial guess
+bnds = ((1E-12, 0.85, 1E-12), (1E-3, 1, 1E-3))
+
+# bnds = ((1E-12, 1E-4, 1), (1E-3, 0.99,100))
+y_ls = least_squares(fcn_residuals, p0, bounds=bnds,
+                     args=(fnc_y, grass.t, t_data, m_data),
+                     kwargs={'plot_progress':True})
+
+# Calibration accuracy
+y_calib_acc = fcn_accuracy(y_ls)
+
+# Run calibrated simulation
+p_hat = y_ls['x']
+WgDM_hat = fnc_y(p_hat)
+
+#### -- Plot results --
+plt.figure(1)
+plt.plot(grass.t, WgDM_hat, label='WgDM')
+plt.plot(t_data, m_data,
+          linestyle='None', marker='o', label='WgDM data')
+plt.legend()
+plt.xlabel(r'$time\ [d]$')
+plt.ylabel(r'$grass\ biomass\ [kgDM\ m^{-2}]$')
+plt.show()
